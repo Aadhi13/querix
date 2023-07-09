15467662c3acfe20a8912fc829efca49b639fb88
Adhil Ameen <67848808+Aadhi13@users.noreply.github.com>
2023-07-09 20:30:23 +0530

    Fix issue #1: Resolved the problem with the User Menu button not closing

--- client/src/components/user/Header/Header.jsx
+++ client/src/components/user/Header/Header.jsx
@@ -36,7 +36,7 @@ function Header() {
             </button>
             <div className='flex items-center px-2'><input onFocus={() => setSearchBoxFocus(true)} onBlur={() => setSearchBoxFocus(false)} className='outline-none  pl-1 w-80' id='searchBox' placeholder='Search' type="text" /></div>
           </div>
-          <div className='flex border items-center gap-2 border-gray-300 rounded-full py-2 px-4' onClick={(e) => { e.stopPropagation(); setMenu(true) }}>
+          <div className='flex border items-center gap-2 border-gray-300 rounded-full py-2 px-4' onClick={(e) => { e.stopPropagation(); setMenu(!menu) }}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
               <path fillRule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd" />
             </svg>
